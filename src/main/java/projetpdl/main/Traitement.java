5924cd26b42c1bbdbfe64c17ead7c4631c52a596
Gora Fall <fallgora.egf@gmail.com>
2018-11-06 18:07:26 +0100

    coomit test de gora

---
+++ src/main/java/projetpdl/main/Traitement.java
@@ -0,0 +1,21 @@
+package projetpdl.main;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.wikipedia.Wiki;
+
+import com.bitplan.mediawiki.japi.Mediawiki;
+import java.io.BufferedReader;
+public class Traitement {
+
+
+    public static void main(String[] args) throws Exception {
+    	
+    	
+	}
+
+}
