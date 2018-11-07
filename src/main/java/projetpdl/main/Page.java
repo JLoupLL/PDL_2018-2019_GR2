7bf086101d24c99a5fb14e5f30069352b4159a43
Gora Fall <fallgora.egf@gmail.com>
2018-11-07 17:14:11 +0100

    deuxiéme commit de gora

--- src/main/java/projetpdl/main/Page.java
+++ src/main/java/projetpdl/main/Page.java
@@ -2,4 +2,19 @@ package projetpdl.main;
 
 public class Page {
 
+	String url;
+
+	public Page(String url) {
+		super();
+		this.url = url;
+	}
+	
+	public String getTitre(){
+		
+		
+			
+		return url;
+		
+		
+	}
 }
