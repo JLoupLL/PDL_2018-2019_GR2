97c2123732cc41b732038c0814dca09e40a3dbfb
momoIB <mohamedibnmrabet@gmail.com>
2018-11-07 18:19:18 +0100

    deuxieme commit Mohamed test

--- src/main/java/projetpdl/main/Extraction.java
+++ src/main/java/projetpdl/main/Extraction.java
@@ -7,14 +7,11 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import org.wikipedia.Wiki;
-
-import com.bitplan.mediawiki.japi.Mediawiki;
 import java.io.BufferedReader;
 
 public class Extraction {
 	//test commit JL 2
-	// test commit momo
+	// test 2 commit momo
 
 	    public static void main(String[] args) throws Exception {
 //	        URL wiki = new URL("https://fr.wikipedia.org/wiki/Comma-separated_values");
