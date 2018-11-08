2db035d4697c55fc8a2e537c996d9109fdb2cb11
JLoupLL <jean-loup.lelec@etudiant.univ-rennes1.fr>
2018-11-08 12:11:11 +0100

    ajouts de méthodes getHtmlJsoup() et getHtmlMediaWiki()

--- src/main/java/projetpdl/main/Extraction.java
+++ src/main/java/projetpdl/main/Extraction.java
@@ -24,85 +24,122 @@ public class Extraction {
 	//test commit JL 2
 	// test 2 commit momo
 
-	    public static void main(String[] args) throws Exception {
-	    	
-//	        URL wiki = new URL("https://fr.wikipedia.org/wiki/Comma-separated_values");
-//	        URLConnection yc = wiki.openConnection();
-//	        BufferedReader in = new BufferedReader(new InputStreamReader(
-//	                                    yc.getInputStream()));
-	        String inputLine = null;
-	        ArrayList<String> list = new ArrayList<String>();
-		    Iterator<String> it =  list.iterator();
-		    FileWriter fichier_contenue = new FileWriter("contenue.csv");
-			int i = 0;
-		    String chaine1 ="https://en.wikipedia.org/wiki/List_of_decades";
-		    String chaine2 ="/table>";
-		    try {
-		    	//https://fr.wikipedia.org/wiki/Classification_de_K%C3%B6ppen
-		    	//http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&titles=Main%20Page&format=csv"
+	public static void main(String[] args) throws Exception {
 
-		    	Mediawiki wiki1= new Mediawiki("https://en.wikipedia.org/wiki/List_of_decades");
-		        //String content = wiki1.getPageContent("Main Page");
-		        //System.out.println(content);
-		        fichier_contenue.append(inputLine);
-		        
-		        String c =  wiki1.getStringFromUrl(chaine1);
-		        System.out.println("test");
-		        System.out.println(c);
-		        int cp = c.length();
-		        
-		        //System.out.println(cp);
-		        while(cp > 0){
-		        	if(c.contains(chaine2)){
-		    	        System.out.println(c);
-		        	}
-		        	cp--;
-		        }
+		//	        URL wiki = new URL("https://fr.wikipedia.org/wiki/Comma-separated_values");
+		//	        URLConnection yc = wiki.openConnection();
+		//	        BufferedReader in = new BufferedReader(new InputStreamReader(
+		//	                                    yc.getInputStream()));
+		String inputLine = null;
+		ArrayList<String> list = new ArrayList<String>();
+		Iterator<String> it =  list.iterator();
+		FileWriter fichier_contenue = new FileWriter("contenue.csv");
+		int i = 0;
+		String chaine1 ="https://en.wikipedia.org/wiki/List_of_decades";
+		String chaine2 ="table>";
+		String chaine3 ="<table";
+		try {
+			//https://fr.wikipedia.org/wiki/Classification_de_K%C3%B6ppen
+			//http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&titles=Main%20Page&format=csv"
 
-		        //System.out.println(wiki1.getPageContent("wiki/List_of_decades"));
-		        
-		       
-		        
-//			    fichier_contenue.append(inputLine);
-//	            list.add(inputLine);
-//			    	}
-//			    while(it.hasNext()){
-//		        	fichier_contenue.append((CharSequence) it);
-//			    }
-//			    while(it.hasNext()){
-//			    	//System.out.println(fichier_contenue);
-//		        	//fichier_contenue.append((CharSequence) it);
-//			    }
-			   
-//	        in.close();	
-	      
-			} catch (Exception e) {
-				// TODO: handle exception
-			}  
-		    
-	        
+			Mediawiki wiki1= new Mediawiki("https://en.wikipedia.org/wiki/List_of_decades");
+			//String content = wiki1.getPageContent("Main Page");
+			//System.out.println(content);
+			fichier_contenue.append(inputLine);
+
+			String c =  wiki1.getStringFromUrl(chaine1);
+			System.out.println("test");
+
+			int cp = c.length();
+
+			//System.out.println(cp);
+			//while(cp > 0){
+			if(c.contains(chaine3)){
+				String test=c.substring(c.indexOf(chaine3),c.indexOf(chaine2)+6);
+				System.out.println(test);
+				System.out.println("test2");
+				//System.out.println(c.substring(0,cp));
+			}
+			cp--;
+			//}
+
+			//System.out.println(wiki1.getPageContent("wiki/List_of_decades"));
+
+
+
+			//			    fichier_contenue.append(inputLine);
+			//	            list.add(inputLine);
+			//			    	}
+			//			    while(it.hasNext()){
+			//		        	fichier_contenue.append((CharSequence) it);
+			//			    }
+			//			    while(it.hasNext()){
+			//			    	//System.out.println(fichier_contenue);
+			//		        	//fichier_contenue.append((CharSequence) it);
+			//			    }
+
+			//	        in.close();	
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}  
+		getContentHtml();
+
+	}
+
+	public static void getContentHtml(){
+		// TODO Auto-generated method stub    https://fr.wikipedia.org/wiki/Championnat_de_France_de_football 
+		Document doc=new Document("test"); 
+		try {
+			//doc = Jsoup.connect("http://espn.go.com/mens-college-basketball/conferences/standings/_/id/2/year/2012/acc-conference").get();
+			doc = Jsoup.connect("https://fr.wikipedia.org/wiki/Championnat_de_France_de_football").get();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	    
-	     public void getContentHtml(){
-	    		// TODO Auto-generated method stub    https://fr.wikipedia.org/wiki/Championnat_de_France_de_football 
-	    		Document doc=new Document("test"); 
-	    		try {
-	    			//doc = Jsoup.connect("http://espn.go.com/mens-college-basketball/conferences/standings/_/id/2/year/2012/acc-conference").get();
-	    			doc = Jsoup.connect("https://fr.wikipedia.org/wiki/Championnat_de_France_de_football").get();
-	    		} catch (IOException e) {
-	    			// TODO Auto-generated catch block
-	    			e.printStackTrace();
-	    		}
-	    		System.out.println("testtstte");
-	        	for (Element table : doc.select("table")) {
-	        		for (Element row : table.select("tr")) { 
-	        			Elements tds = row.select("td");
-	        			if (tds.size() > 6) {
-	        				System.out.println(tds.get(0).text() + ":" + tds.get(1).text());     
-	        			}
-	        		}
-	        	}
-	    	
+		System.out.println("testtstte");
+		for (Element table : doc.select("table")) {
+			for (Element row : table.select("tr")) { 
+				Elements tds = row.select("td");
+				if (tds.size() > 1) {
+					System.out.println(tds.get(0).text() + ":" + tds.get(1).text() + ":" + tds.get(2).text());     
+				}
+			}
+		}
+	}
+	public String getHtmlJsoup(String url) {
+		String retour= "";
+		Document doc=new Document("test");
+		try {
+			doc = Jsoup.connect(url).get();
+			doc = Jsoup.connect("https://fr.wikipedia.org/wiki/Championnat_de_France_de_football").get();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		retour=doc.toString();
 
-	    }
+		return retour;
+	}
+	public String getHtmlMediaWiki(String url) {
+		String retour="";
+		Mediawiki wiki1 = null;
+		try {
+			wiki1= new Mediawiki("test");
+		} 
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		retour=wiki1.getStringFromUrl(url);
+		retour=wiki1.getStringFromUrl("https://fr.wikipedia.org/wiki/Championnat_de_France_de_football");
+
+		return retour;	    	 
+	}
+	public String getHtmlWikiData(String url) {
+		//A FAIRE
+		return "";
+	}
+
 }
