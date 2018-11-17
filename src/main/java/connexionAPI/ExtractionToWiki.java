6124950394d21799732eccad88370e004a3581ee
Gora Fall <fallgora.egf@gmail.com>
2018-11-17 01:39:30 +0100

    commit debut solution du wikiText

--- src/main/java/connexionAPI/ExtractionToWiki.java
+++ src/main/java/connexionAPI/ExtractionToWiki.java
@@ -6,6 +6,8 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.regex.Pattern;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,6 +15,7 @@ import java.io.Serializable;
 import java.io.File;
 import java.io.IOException;
 
+import org.eclipse.persistence.internal.oxm.schema.model.List;
 import org.json.JSONArray;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -45,28 +48,32 @@ public class ExtractionToWiki {
              
              //Fonction pour essayeer de Parcer le wikiText avec Parse
              public static void getTabeWiki(String chaine) {//que fait cette fonction?
-                           String prefix ="|";       
-                           String text="{|"+chaine+"|}"; 
+                           ArrayList<String> listTAbleau = new ArrayList<String>();
+            	 			String prefix ="|";       
+                           String text="{"+chaine+"}"; 
                            Pattern p = Pattern.compile("<[^>]+>"); 
                            java.util.regex.Matcher m = p.matcher(text); 
                            String result =""; 
 
                            while(m.find()) { 
 
-                                        result = m.replaceAll("");
-                                        
-                                        System.out.println("\ntest:");
-                                        int cpt = result.length();
-                                        System.out.println(result);   //ici on récupère tout le wikicode de la page je crois
+                                        result = m.replaceAll("");                                      
+                                        System.out.println("\ntest:");                                             		
+                                        System.out.println(result);              
+                                        //ici on récupère tout le wikicode de la page je crois
                                         System.out.println("\ntest2:");
-                           } 
+                           }                      
              }
              //Recupération contenue wikitext
-             public static void getContenuePage(String url) throws IOException {
+             public static String getContenuePage(String url) throws IOException {
                                         String balisedebut ="{|";
                                         String baliseFin   = "|}";
                                         String nvligne     = "\n";
+                     String jsonUrl = url+"/w/api.php?action=parse&page=\"+titre+\"&prop=wikitext&format=json";
+                     
                      URL wikipedia = new URL(url);
+                    // "https://en.wikipedia.org/w/api.php?action=parse&page=Comparison_of_Canon_EOS_digital_cameras&prop=wikitext&format=json"
+                     StringBuilder result = new StringBuilder();
                      URLConnection yc = wikipedia.openConnection();
                      BufferedReader in = new BufferedReader(
                                              new InputStreamReader(
@@ -75,57 +82,51 @@ public class ExtractionToWiki {
              
                      while ((inputLine = in.readLine()) != null) 
                      {
-                          getTabeWiki(inputLine); //appel  de la fonction getTablexiki 
+                    	 result.append(inputLine);     	 
+                    	 //getTabeWiki(result..append(inputLine+"\n"));
+                    	 //getTabeWiki(inputLine); //appel  de la fonction getTablexiki 
                      }
                      in.close();
+                     return result.toString();
+                    // return  wikipedia.getUserInfo();
              }
-             
-             public static void main(String[] args) throws Exception {
-                           
-                           String Url ="https://en.wikipedia.org/wiki/Comparison_of_Canon_EOS_digital_cameras";
-                          
-                           getContenuePage(Url);
-                           
-             }
-}
-
-
-
-<<<<<<< HEAD
-			result = m.replaceAll("");
-			
-			System.out.println("\ntest:");
-			int cpt = result.length();
-			System.out.println(result);   //ici on récupère tout le wikicode de la page je crois
-			System.out.println("\ntest2:");
-		} 
-	}
-	//Recupération contenue wikitext
-	public static void getContenuePage(String url) throws IOException {
-			String balisedebut ="{|";
-			String baliseFin   = "|}";
-			String nvligne     = "\n";
-	        URL wikipedia = new URL(url);
-	        URLConnection yc = wikipedia.openConnection();
-	        BufferedReader in = new BufferedReader(
-	                                new InputStreamReader(
-	                                yc.getInputStream()));
-	        String inputLine;
-	 
-	        while ((inputLine = in.readLine()) != null) 
-	        {
-	        	getTabeWiki(inputLine); //appel  de la fonction getTablexiki 
-	        }
-	        in.close();
-	}
-	
+          //formater par ligne du tableau le contenu
+		  public static ArrayList<String> recupLineTable(String chaine)
+		    {
+		    	 ArrayList<String> resultat = new ArrayList<String>(); 
+		    	char sepateurLine = '|';
+		 		for(int i = 0;i<chaine.length();i++ )
+		 		{
+		 			if(chaine.charAt(i) ==  sepateurLine)
+		 			{
+		 				System.out.println("");
+		 			}else
+		 			{
+		 				System.out.print(chaine.charAt(i));
+		 				
+		 			}		 			
+		 		}
+				return resultat;
+		    }
+					
 	public static void main(String[] args) throws Exception {
 		
-		String Url ="https://en.wikipedia.org/wiki/Comparison_of_Canon_EOS_digital_cameras";
+		String Url1 ="https://en.wikipedia.org/w/api.php?action=parse&page=Comparison_of_Canon_EOS_digital_cameras&prop=wikitext&format=json";
 		
-		getContenuePage(Url);
+		String Url3 ="https://fr.wikipedia.org//w/api.php?action=parse&page=Liste_des_pays_par_PIB_nominal&prop=wikitext&format=json";
 		
+		//System.out.println(wiki.fromXML(getContenuePage(Url)));
+		//System.out.println(getContenuePage(Url));
+		
+		
+		
+		String lis1 = getContenuePage(Url1);
+		
+		recupLineTable(lis1);
+//		Iterator<String> it = lis1.iterator();
+//		while(it.hasNext())
+//		{
+//			
+//		}
 	}
 }
-=======
->>>>>>> e67ff1796a29925fd3abb1db93b2b1f33886b464
