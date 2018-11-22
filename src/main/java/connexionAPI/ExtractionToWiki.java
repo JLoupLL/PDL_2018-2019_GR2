83d88a04f9f87043e38ebe6aa626d8e327dbe0a5
Gora Fall <fallgora.egf@gmail.com>
2018-11-22 17:52:51 +0100

    code wiki

--- src/main/java/connexionAPI/ExtractionToWiki.java
+++ src/main/java/connexionAPI/ExtractionToWiki.java
@@ -3,7 +3,16 @@ package connexionAPI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.regex.Pattern;
+
+import org.eclipse.persistence.internal.oxm.schema.model.Content;
+import org.json.JSONObject;
+import org.wikipedia.Mediawiki;
+
+import com.sun.xml.txw2.Document;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,26 +20,26 @@ import java.io.InputStreamReader;
 public class ExtractionToWiki {
 
 	// Fonction pour essayeer de Parcer le wikiText avec Parse
-	public static void getTabeWiki(String chaine) {// que fait cette fonction?
-		String text = "{" + chaine + "}";
-		Pattern p = Pattern.compile("<[^>]+>");
-		java.util.regex.Matcher m = p.matcher(text);
-		String result = "";
+//	public static void getTabeWiki(String chaine) {// que fait cette fonction?
+//		String text = "{" + chaine + "}";
+//		//\\{(.*?)\\}", "
+//		Pattern p = Pattern.compile("\\{(.*?)\\}\",");
+//		java.util.regex.Matcher m = p.matcher(text);
+//		String result = "";
+//
+//		while (m.find()) {
+//
+//			result = m.replaceAll("");
+//			System.out.println("\ntest:");
+//			System.out.println(result);
+//			// ici on récupère tout le wikicode de la page je crois
+//			System.out.println("\ntest2:");
+//		}
+//	}
 
-		while (m.find()) {
-
-			result = m.replaceAll("");
-			System.out.println("\ntest:");
-			System.out.println(result);
-			// ici on récupère tout le wikicode de la page je crois
-			System.out.println("\ntest2:");
-		}
-	}
-
-	// Recupération contenue wikitext
+	// Recupération contenue wikitext en format Json
 	public static String getContenuePage(String url) throws IOException {
 		URL wikipedia = new URL(url);
-		// "https://en.wikipedia.org/w/api.php?action=parse&page=Comparison_of_Canon_EOS_digital_cameras&prop=wikitext&format=json"
 		StringBuilder result = new StringBuilder();
 		URLConnection yc = wikipedia.openConnection();
 		BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
@@ -38,26 +47,45 @@ public class ExtractionToWiki {
 
 		while ((inputLine = in.readLine()) != null) {
 			result.append(inputLine);
-			// getTabeWiki(result..append(inputLine+"\n"));
-			// getTabeWiki(inputLine); //appel de la fonction getTablexiki
 		}
 		in.close();
 		return result.toString();
-		// return wikipedia.getUserInfo();
+	}
+	//cette fonction format le contenu en format tableau wikitext 
+	public static String getTableFormatwikitext(String json) {
+		String content = "";
+		try {
+			JSONObject objetJson = new JSONObject(json);
+			String docs = objetJson.getString("parse");
+			JSONObject objetJson2 = new JSONObject(docs);
+			String wikitext = objetJson2.getString("wikitext");
+			JSONObject objetJson3 = new JSONObject(wikitext);
+			content = objetJson3.getString("*");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	return content;
 	}
 
 	// formater par ligne du tableau le contenu
-	public static ArrayList<String> recupLineTable(String chaine) {
-		ArrayList<String> resultat = new ArrayList<String>();
-		char sepateurLine = '|';
+	public static ArrayList recupLineTable(String chaine) {
+		ArrayList resultat = new ArrayList<String>();
+		String  [] ligne = null;
+		List characters = new ArrayList();
+        List contentList = new ArrayList();
+        char nligne ='\n'+' ';
+		char currentChar = '|';
+		char currentCharEst =' ';
 		for (int i = 0; i < chaine.length(); i++) {
-			if (chaine.charAt(i) == sepateurLine) {
-				System.out.println("");
-			} else {
+			if (chaine.charAt(i) == currentChar) {
+				System.out.println(chaine.charAt(i));
+			}else 					
+			{
+				currentCharEst = chaine.charAt(i);
+				resultat.add(currentCharEst);
 				System.out.print(chaine.charAt(i));
-
 			}
-		}
+		}	
 		return resultat;
 	}
 
@@ -65,18 +93,17 @@ public class ExtractionToWiki {
 
 		String Url1 = "https://en.wikipedia.org/w/api.php?action=parse&page=Comparison_of_Canon_EOS_digital_cameras&prop=wikitext&format=json";
 
-		//String Url3 = "https://fr.wikipedia.org//w/api.php?action=parse&page=Liste_des_pays_par_PIB_nominal&prop=wikitext&format=json";
-
+		String Url3 = "https://fr.wikipedia.org//w/api.php?action=parse&page=Liste_des_pays_par_PIB_nominal&prop=wikitext&format=json";
 		// System.out.println(wiki.fromXML(getContenuePage(Url)));
 		// System.out.println(getContenuePage(Url));
-
 		String lis1 = getContenuePage(Url1);
-
-		recupLineTable(lis1);
-		// Iterator<String> it = lis1.iterator();
-		// while(it.hasNext())
-		// {
-		//
-		// }
+		System.out.println(getTableFormatwikitext(lis1));
+//		ArrayList list = recupLineTable(getTableFormatwikitext(lis1));
+//		Iterator<String> it = list.iterator();
+//		while(it.hasNext())
+//		{
+//			System.out.print(it.next());
+//		}
+		
 	}
 }
