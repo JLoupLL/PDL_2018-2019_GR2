a0a0acc467a6a2c85dc8d744239a0121abec2929
JLoupLL <jean-loup.lelec@etudiant.univ-rennes1.fr>
2018-11-26 15:12:12 +0100

    amélioration de l'extraction, les tableaux pertinents sont récupérés

--- src/main/java/connexionAPI/ExtractionToHTML.java
+++ src/main/java/connexionAPI/ExtractionToHTML.java
@@ -11,17 +11,13 @@ import org.jsoup.select.Elements;
 
 public class ExtractionToHTML {
 	private String url;
-	
+
 	public ExtractionToHTML(String url) {
 		this.url = url;
 	}
 
 	public static void main(String[] args) {
-		ExtractionToHTML html = new ExtractionToHTML("https://en.wikipedia.org/wiki/Comparison_of_Canon_EOS_digital_cameras");
-		html.getContentHtml();
-		html.setUrl("https://fr.wikipedia.org/wiki/Championnat_de_France_de_football");
-		html.getContentHtml();
-		html.setUrl("https://fr.wikipedia.org/wiki/Loi_des_Douze_Tables");
+		ExtractionToHTML html = new ExtractionToHTML("https://en.wikipedia.org/wiki/Comparison_between_U.S._states_and_countries_by_GDP_(PPP)");
 		html.getContentHtml();
 	}
 
@@ -50,7 +46,7 @@ public class ExtractionToHTML {
 		// dans des tableaux
 		insertionDonnesTableauDansFichierCSV(doc, fileWriter);
 	}
-	
+
 	public void urltrue(String url) throws IOException {
 		if ( !url.contains("https://en.wikipedia.org") || !url.contains("https://fr.wikipedia.org") ){
 			throw new IOException();
@@ -60,7 +56,7 @@ public class ExtractionToHTML {
 	public Document getHtmlJsoup(String url) {
 		Document doc = null; //
 		try {
-			urltrue(url);
+			//urltrue(url);
 			doc = Jsoup.connect(url).get();
 		} catch (IOException e) {
 			System.out.println("erreur lors de la r�cup�ration du code html");
@@ -68,9 +64,9 @@ public class ExtractionToHTML {
 		}
 		return doc;
 	}
-
+	//plus besoin de cette fonction, elle est donnee par les profs (cf norme de nommage)
 	private FileWriter creationFichierCsv(Document doc) { // peut �tre d�placer dans un autre package
-																// (createFileCSV)
+		// (createFileCSV)
 		// Cr�ation du fichier csv avec comme titre le premier h1 de la page wikip�dia
 		Elements titre = doc.select("h1");
 		FileWriter fileWriter = null;
@@ -84,22 +80,29 @@ public class ExtractionToHTML {
 	}
 
 	private void insertionDonnesTableauDansFichierCSV(Document doc, FileWriter fileWriter) {
-		
+
 		// parcours des tableaux de la page
 		// attention peut-�tre un probl�me?? si on est sur une page wikip�dia contenant
 		// le mot "table"
 		for (Element table : doc.select("table")) {
-			for (Element row : table.select("tr")) {
-				String ligneDunTableau = "";
-				for (Element td : row.select("td")) {
-					ligneDunTableau += td.text().trim() + ";";
-				}
-				try {
-					fileWriter.append(ligneDunTableau);
-					fileWriter.append("\n");
-				} catch (IOException e) {
-					System.out.println("erreur lors de l'ajout d'une ligne dans le fichier .CSV");
-					e.printStackTrace();
+			System.out.println(table.className());
+			if (table.className().contains("wikitable")) {
+
+				for (Element row : table.select("tr")) {
+					String ligneDunTableau = "";
+					for(Element th : row.select("th")) {
+						ligneDunTableau += th.text().trim() + ";";
+					}
+					for (Element td : row.select("td")) {
+						ligneDunTableau += td.text().trim() + ";";
+					}
+					try {
+						fileWriter.append(ligneDunTableau);
+						fileWriter.append("\n");
+					} catch (IOException e) {
+						System.out.println("erreur lors de l'ajout d'une ligne dans le fichier .CSV");
+						e.printStackTrace();
+					}
 				}
 			}
 		}
