26c42d7f19146db46ccb97805eb3484f1aed90b0
Gora Fall <fallgora.egf@gmail.com>
2018-12-07 11:06:31 +0100

    bon code

--- wikipedia-parser-0.1/src/java/be/devijver/wikipedia/SmartLinkResolver.java
+++
@@ -1,6 +0,0 @@
-package be.devijver.wikipedia;
-
-public interface SmartLinkResolver {
-
-	String resolve(String smartLink);
-}
--- wikipedia-parser-0.1/src/java/be/devijver/wikipedia/html/HtmlEncoder.java
+++
@@ -1,6 +0,0 @@
-package be.devijver.wikipedia.html;
-
-public interface HtmlEncoder {
-
-	String encode(String s);
-}
